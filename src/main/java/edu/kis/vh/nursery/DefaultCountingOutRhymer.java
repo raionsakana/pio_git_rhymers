e38d7e3993f7c4051560f1f61d2ab7a08891709f
Michał Ogłoza <michalogloza97@gmail.com>
2019-11-26 14:49:19 +0100

    Stałe i annotacje 1

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,9 +2,14 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private int[] numbers = new int[12];
+    public static final int TWELVE = 12;
+    public static final int MINUS_ONE = -1;
+    public static final int MAX = 11;
 
-    public int total = -1;
+
+    private int[] numbers = new int[TWELVE];
+
+    public int total = MINUS_ONE;
 
     public void countIn(int in) {
         if (!isFull())
@@ -12,22 +17,22 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean check() {
-        return total == -1;
+        return total == MINUS_ONE;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return total == MAX;
     }
 
     protected int peek() {
         if (check())
-            return -1;
+            return MINUS_ONE;
         return numbers[total];
     }
 
     public int countOut() {
         if (check())
-            return -1;
+            return MINUS_ONE;
         return numbers[total--];
     }
 
