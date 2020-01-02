c28345fa86d5fbd03b66fb1721c3d05451f5d74e
raionsakana <52761148+raionsakana@users.noreply.github.com>
2020-01-02 23:32:25 +0100

    zad_1_5_1-2

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -6,28 +6,40 @@ import edu.kis.vh.nursery.factory.RhymersFactory;
 class RhymersDemo {
 
 	public static void main(String[] args) {
-		RhymersFactory factory = new DefaultRhymersFactory();
-		
-		DefaultCountingOutRhymer[] rhymers = { factory.GetStandardRhymer(), factory.GetFalseRhymer(),
-				factory.GetFIFORhymer(), factory.GetHanoiRhymer()};
-		
+		DefaultRhymersFactory factory = new DefaultRhymersFactory();
+		testRhymers(factory);
+	}
+
+	private static void testRhymers(RhymersFactory factory) {
+
+		DefaultCountingOutRhymer[] rhymers = prepareDefaultCountingOutRhymers(factory);
+		java.util.Random rn = new java.util.Random();
+
 		for (int i = 1; i < 15; i++)
 			for (int j = 0; j < 3; j++)
 				rhymers[j].countIn(i);
-		
-		java.util.Random rn = new java.util.Random();
+
 		for (int i = 1; i < 15; i++)
 			rhymers[3].countIn(rn.nextInt(20));
-		
+
 		for (int i = 0; i < rhymers.length; i++) {
-			while (!rhymers[i].callCheck())
+			while (!rhymers[i].check())
 				System.out.print(rhymers[i].countOut() + "  ");
 			System.out.println();
 		}
-		
-		System.out.println("total rejected is "
-				+ ((HanoiRhymer) rhymers[3]).reportRejected());
-		
+
+		System.out.println("total rejected is " + ((HanoiRhymer) rhymers[3]).reportRejected());
 	}
-	
+
+	private static DefaultCountingOutRhymer [] prepareDefaultCountingOutRhymers(RhymersFactory factory) {
+		DefaultCountingOutRhymer[] rhymers  = {
+				factory.getStandardRhymer(),
+				factory.getFalseRhymer(),
+				factory.getFIFORhymer(),
+				factory.getHanoiRhymer()
+		};
+
+		return rhymers;
+	}
+
 }
